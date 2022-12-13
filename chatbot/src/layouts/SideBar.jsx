4f7490a0fe2f40b32e49a716c40cdf123f797b76
Sangeetha M <sangeetha.m@bzanalytics.ai>
2022-12-13 16:47:50 +0530

    Worked Untill FAQ component

---
+++ chatbot/src/layouts/SideBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+
+
+const SideBar = () => {
+  return (
+    <div>
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+      
+        
+      </Box>
+    </div>
+  );
+}
+
+export default SideBar
